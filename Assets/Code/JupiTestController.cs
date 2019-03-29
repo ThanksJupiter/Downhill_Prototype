6df8bd67e4d5b7fc367b9ee2a55e1b66b557a5d0
Kasper Enwall <karl.kasper.enwall@hotmail.com>
2019-03-29 14:48:37 +0100

    Less physics based controller
    FuskController

---
+++ Assets/Code/JupiTestController.cs
@@ -0,0 +1,214 @@
+﻿using UnityEngine;
+
+public class JupiTestController : MonoBehaviour
+{
+    #region Properties
+    [SerializeField] private float speedMultiplier = 5f;
+    [SerializeField] private float rotationSpeed = 200f;
+    [SerializeField] private float mass = 100f;
+    [SerializeField] private Vector3 acceleration;
+
+    [SerializeField] private float tiltFloat;
+    [SerializeField] private float maxVelocity = 50f;
+    [SerializeField] private float gravity = -9.82f;
+    [SerializeField] private float checkGroundDistance = 1f;
+    [SerializeField] private float friction;
+
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] private float velocityMagnitude;
+    [SerializeField] private Vector3 lastFrameVelocity;
+
+    [SerializeField] private Vector2 inputVector;
+
+    [SerializeField] private Vector3 projectedVector;
+    [SerializeField] private Vector3 desiredTravelDirection;
+
+    [Range(0, 1)]
+    public float alpha;
+
+    bool isGrounded;
+    #endregion Properties
+
+    #region Delegates
+    delegate void OnLandedDelegate();
+    OnLandedDelegate OnLanded;
+
+    delegate void OnLeftGroundDelegate();
+    OnLeftGroundDelegate OnLeftGround;
+
+    private void OnEnable()
+    {
+        OnLanded += BroadcastLanded;
+        OnLeftGround += BroadcastLeftGround;
+    }
+
+    private void OnDisable()
+    {
+        OnLanded -= BroadcastLanded;
+        OnLeftGround -= BroadcastLeftGround;
+    }
+    #endregion Delegates
+
+    #region Unity Updates
+    void Update()
+    {
+        // TODO set tilt float
+        inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        CheckGround();
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, checkGroundDistance))
+        {
+            if (isGrounded)
+            {
+                //velocity.y = projectedVector.y;
+            }
+            CalculateDesiredTravelDirection(hit);
+            velocityMagnitude = velocity.magnitude;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        transform.rotation = CalculateRotation();
+    }
+
+    private void FixedUpdate()
+    {
+        velocity += CalculateAcceleration();
+        if (isGrounded)
+        {
+            velocity.y = desiredTravelDirection.y;
+        }
+
+        transform.position += velocity * speedMultiplier * Time.fixedDeltaTime;
+    }
+    #endregion Unity Updates
+
+    #region Physics? calculations
+    private Vector3 CalculateVelocity()
+    {
+        return velocity;
+    }
+
+    private Vector3 CalculateAcceleration()
+    {
+        if (isGrounded)
+        {
+            acceleration = desiredTravelDirection / mass;
+            //velocity += desiredTravelDirection / mass;
+        }
+
+        return acceleration;
+    }
+
+    private void CalculateDesiredTravelDirection(RaycastHit hit)
+    {
+        Vector3 normal = hit.normal;
+        //Vector3 point = hit.point;
+
+        Vector3 netForce = -transform.up + normal;
+
+        projectedVector = Vector3.ProjectOnPlane(-transform.up, hit.normal);
+        Vector3 flatProject = projectedVector;
+        flatProject.y = 0;
+
+        float forwardDot = Vector3.Dot(flatProject.normalized, transform.forward);
+        float rightDot = Vector3.Dot(flatProject.normalized, transform.right);
+
+        //desiredTravelDirection = (transform.position + netForce) - transform.position;
+        desiredTravelDirection = Vector3.ProjectOnPlane(-transform.up, normal);
+        desiredTravelDirection = Quaternion.AngleAxis(rightDot * -Mathf.Rad2Deg, hit.normal) * desiredTravelDirection;
+
+        desiredTravelDirection.y *= new Vector3(velocity.x, 0f, velocity.z).magnitude;
+
+        //         Vector3 groundCheckDirection = -transform.up * checkGroundDistance;
+        //         bool facingUp = false;
+        // 
+        //         projectedVector = Vector3.ProjectOnPlane(groundCheckDirection, hit.normal);
+        //         Vector3 flatProject = projectedVector;
+        //         flatProject.y = 0;
+        //         float forwardDot = Vector3.Dot(flatProject.normalized, transform.forward);
+        //         float rightDot = Vector3.Dot(flatProject.normalized, transform.right);
+        // 
+        //         facingUp = forwardDot < 0;
+        // 
+        //         desiredTravelDirection = new Vector3(transform.forward.x * projectedVector.x, projectedVector.y * forwardDot, transform.forward.z * projectedVector.z);
+        //         desiredTravelDirection = Quaternion.AngleAxis(rightDot * -Mathf.Rad2Deg, hit.normal) * desiredTravelDirection;
+        // 
+        //         if (facingUp)
+        //         {
+        //             desiredTravelDirection = Quaternion.AngleAxis(180, projectedVector) * -desiredTravelDirection;
+        //         }
+        // 
+        //         desiredTravelDirection = Vector3.ProjectOnPlane(desiredTravelDirection, hit.normal);
+        //         desiredTravelDirection.Normalize();
+    }
+
+    private Quaternion CalculateRotation()
+    {
+        float theRightDot = Vector3.Dot(transform.right, velocity);
+
+        Vector3 eulerAngleVelocity = new Vector3(0f, inputVector.x * rotationSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(transform.rotation.eulerAngles + eulerAngleVelocity * Time.deltaTime);
+
+        return deltaRotation;
+    }
+    #endregion Physics? calculations
+
+    #region State checks
+    private void CheckGround()
+    {
+        bool tmpLanded = Physics.Raycast(transform.position, -transform.up, checkGroundDistance);
+
+        if (!isGrounded && tmpLanded)
+        {
+            OnLanded?.Invoke();
+        }
+
+        if (isGrounded && !tmpLanded)
+        {
+            OnLeftGround?.Invoke();
+        }
+
+        isGrounded = tmpLanded;
+    }
+    #endregion State checks
+
+    #region Event broadcasts
+    private void BroadcastLanded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, checkGroundDistance))
+        {
+            CalculateDesiredTravelDirection(hit);
+            //velocity.y = desiredTravelDirection.y * new Vector3(velocity.x, 0f, velocity.z).magnitude;
+            print("setting velocity.y: " + velocity.y);
+        }
+
+        //print("OnLanded!, velocity: " + velocity + " desired travel dir: " + desiredTravelDirection);
+    }
+
+    private void BroadcastLeftGround()
+    {
+        print("OnLeftGround!, velocity: " + velocity + " desired travel dir: " + desiredTravelDirection);
+    }
+    #endregion Event broadcasts
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + projectedVector * 10f);
+        Gizmos.DrawWireSphere(transform.position + projectedVector * 10f, .2f);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + desiredTravelDirection * 5f);
+        Gizmos.DrawWireSphere(transform.position + desiredTravelDirection * 5f, .3f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + velocity);
+        Gizmos.DrawWireSphere(transform.position + velocity, .4f);
+    }
+}
---
+++ Assets/Code/TotalFuskController.cs
@@ -0,0 +1,235 @@
+﻿using UnityEngine;
+
+public class TotalFuskController : MonoBehaviour
+{
+    #region Properties
+    [Header("Settings")]
+    [SerializeField] private float speedMultiplier = 5f;
+    [SerializeField] private float boardRotationSpeed = 1000f;
+    [SerializeField] private float travelDirectionLerpSpeed = 200f;
+    [SerializeField] private float startTravelThreshold = .2f;
+
+    [SerializeField] private float tiltFloat;
+    [SerializeField] private float maxVelocity = 50f;
+    [SerializeField] private float gravity = -9.82f;
+    [SerializeField] private float friction;
+
+    [SerializeField] private float checkGroundDownDistance = 10f;
+
+    [SerializeField] private float stationaryVelocityThreshold = 2f;
+
+    [Header("Statistics")]
+    [SerializeField] private Vector3 inputVector;
+    [SerializeField] private Vector3 horizontalTravelDirection;
+    [SerializeField] private float velocityMagnitude;
+
+    [SerializeField] private Vector3 boardDirection;
+
+    [SerializeField] private Vector3 currentSlopeDirection;
+    [SerializeField] private float currentSlopeAngle;
+
+    [SerializeField] private float faceTowardSlopeAmount;
+
+    [SerializeField] private bool isGrounded;
+
+    #region Delegates
+    delegate void OnLandedDelegate();
+    OnLandedDelegate OnLanded;
+
+    delegate void OnLeftGroundDelegate();
+    OnLeftGroundDelegate OnLeftGround;
+
+    private void OnEnable()
+    {
+        OnLanded += BroadcastLanded;
+        OnLeftGround += BroadcastLeftGround;
+    }
+
+    private void OnDisable()
+    {
+        OnLanded -= BroadcastLanded;
+        OnLeftGround -= BroadcastLeftGround;
+    }
+    #endregion Delegates
+
+    [System.Serializable]
+    public enum State {
+        Travel,
+        Stationary
+    }
+
+    [SerializeField] private State currentState;
+
+    #endregion Properties
+
+    #region Unity Updates
+    private void Start()
+    {
+        EnterStationaryState();
+    }
+
+    void Update()
+    {
+        if (velocityMagnitude < stationaryVelocityThreshold)
+        {
+            EnterStationaryState();
+        }/* else if (currentState == State.Stationary && velocityMagnitude > stationaryVelocityThreshold)
+        {
+            currentState = State.Travel;
+        }*/
+
+        CheckGround();
+
+        inputVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        inputVector.Normalize();
+
+        print(currentState);
+        switch (currentState)
+        {
+            case State.Travel:
+                ExecuteTravelState();
+                break;
+            case State.Stationary:
+                ExecuteStationaryState();
+                break;
+            default:
+                break;
+        }
+
+        // lean direction
+
+        if (inputVector.magnitude >= .8f)
+        {
+            boardDirection = inputVector;
+        }
+
+        //horizontalTravelDirection = inputVector;
+
+        Debug.DrawLine(transform.position, transform.position + horizontalTravelDirection, Color.blue);
+        Debug.DrawLine(transform.position + horizontalTravelDirection, transform.position + horizontalTravelDirection + -transform.up * checkGroundDownDistance, Color.green);
+    }
+
+    #endregion Unity Updates
+
+    #region Physics? calculations
+
+    private Quaternion CalculateRotation()
+    {
+        float theRightDot = Vector3.Dot(transform.right, boardDirection);
+
+        Vector3 eulerAngleVelocity = new Vector3(0f, theRightDot * boardRotationSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(transform.rotation.eulerAngles + eulerAngleVelocity * Time.deltaTime);
+
+        return deltaRotation;
+    }
+
+    private void RotateBoard()
+    {
+        transform.rotation = CalculateRotation();
+    }
+
+    private void MoveBoarder(Vector3 newPosition)
+    {
+        transform.position = newPosition;
+    }
+
+    #endregion Physics? calculations
+
+    #region State functions
+    private void ExecuteTravelState()
+    {
+        RotateBoard();
+        print("Execute travel state");
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + horizontalTravelDirection * velocityMagnitude * Time.deltaTime, -transform.up, out hit, checkGroundDownDistance))
+        {
+            currentSlopeDirection = Vector3.ProjectOnPlane(-transform.up, hit.normal);
+            Vector3 flatSlopeDirection = new Vector3(currentSlopeDirection.x, 0f, currentSlopeDirection.z);
+            currentSlopeAngle = Vector3.Angle(currentSlopeDirection, flatSlopeDirection);
+
+            horizontalTravelDirection = hit.point - transform.position;
+            horizontalTravelDirection.y = 0f;
+            horizontalTravelDirection.Normalize();
+
+            faceTowardSlopeAmount = Vector3.Dot(boardDirection, flatSlopeDirection);
+            velocityMagnitude += speedMultiplier * Time.deltaTime * faceTowardSlopeAmount;
+            print(velocityMagnitude);
+
+            horizontalTravelDirection = Vector3.Lerp(horizontalTravelDirection, boardDirection, travelDirectionLerpSpeed * Time.deltaTime);
+        }
+
+
+        transform.position = hit.point + transform.up * 1f;
+    }
+
+    private void ExecuteStationaryState()
+    {
+        RotateBoard();
+
+        Vector3 flatSlopeDirection = new Vector3(currentSlopeDirection.x, 0f, currentSlopeDirection.z);
+        faceTowardSlopeAmount = Vector3.Dot(boardDirection, flatSlopeDirection);
+
+        if (Mathf.Abs(faceTowardSlopeAmount) > startTravelThreshold)
+        {
+            EnterTravelState();
+        }
+    }
+
+    private void EnterTravelState()
+    {
+        currentState = State.Travel;
+        horizontalTravelDirection = boardDirection;// * forwards/backwards inverted special variable 
+    }
+
+    private void EnterStationaryState()
+    {
+        currentState = State.Stationary;
+    }
+    #endregion State functions
+
+    #region State checks
+    private void CheckGround()
+    {
+        bool tmpLanded = Physics.Raycast(transform.position, -transform.up, checkGroundDownDistance);
+
+        if (!isGrounded && tmpLanded)
+        {
+            OnLanded?.Invoke();
+        }
+
+        if (isGrounded && !tmpLanded)
+        {
+            OnLeftGround?.Invoke();
+        }
+
+        isGrounded = tmpLanded;
+    }
+    #endregion State checks
+
+    #region Event broadcasts
+    private void BroadcastLanded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + horizontalTravelDirection * velocityMagnitude * Time.deltaTime, -transform.up, out hit, checkGroundDownDistance))
+        {
+            currentSlopeDirection = Vector3.ProjectOnPlane(-transform.up, hit.normal);
+        }
+    }
+
+    private void BroadcastLeftGround()
+    {
+        
+    }
+    #endregion Event broadcasts
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + horizontalTravelDirection);
+        Gizmos.DrawWireSphere(transform.position + horizontalTravelDirection, .4f);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(transform.position, transform.position + currentSlopeDirection * 5f);
+        Gizmos.DrawWireSphere(transform.position + currentSlopeDirection * 5f, .3f);
+    }
+}
